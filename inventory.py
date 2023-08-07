8620912ab983c4ac4841ffeaef4b63e57bc5dda3
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-07 14:35:45 -0500

    Add logging template, fix README formatting.

--- inventory.py
+++ inventory.py
@@ -1,19 +1,30 @@
-# Revisit this.
+#!/usr/bin/env python3
+# inventory.py — An exercise in understanding dictionaries.
+# For more information, see README.md
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="logging.txt",
+    format="%(asctime)s -  %(levelname)s -  %(message)s",
+)
+logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
-def displayInventory(inventory):
+def display_inventory(inventory):
     print("Inventory:")
     item_total = 0
     for k, v in sorted(inventory.items()):
         if k != "dagger":
-            print(str(v) + " " + k)
+            print(str(v), k)
     for k, v in inventory.items():
         if k == "dagger":
-            print(str(v) + " " + k)
+            print(str(v), k)
         item_total += v
-    print("Total number of items: " + str(item_total))
+    print(f"Total number of items: {item_total}")
 
 
 stuff = {"rope": 1, "torch": 6, "gold coin": 42, "dagger": 1, "arrow": 12}
 
-displayInventory(stuff)
+display_inventory(stuff)
