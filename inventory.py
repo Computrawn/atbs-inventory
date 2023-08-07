de6767ced93407c50a552c97fe76f26027f9d6d4
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-07 14:45:46 -0500

    Format stuff list.

--- inventory.py
+++ inventory.py
@@ -12,6 +12,15 @@ logging.basicConfig(
 logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
+stuff = {
+    "rope": 1,
+    "torch": 6,
+    "gold coin": 42,
+    "dagger": 1,
+    "arrow": 12,
+}
+
+
 def display_inventory(inventory):
     print("Inventory:")
     item_total = 0
@@ -24,7 +33,5 @@ def display_inventory(inventory):
         item_total += v
     print(f"Total number of items: {item_total}")
 
-
-stuff = {"rope": 1, "torch": 6, "gold coin": 42, "dagger": 1, "arrow": 12}
 
 display_inventory(stuff)
